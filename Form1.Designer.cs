767ffbab4af9f9e7c09ed0ebd20c311f5ac1303b
Marcos-Tenório <marcos.antoniotaf@gmail.com>
2023-08-23 03:44:33 -0300

    CRUD tecnologia antiga

---
+++ Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbMarca = new System.Windows.Forms.TextBox();
+            this.tbModelo = new System.Windows.Forms.TextBox();
+            this.tbAno = new System.Windows.Forms.TextBox();
+            this.tbCor = new System.Windows.Forms.TextBox();
+            this.tbValor = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbCategoria = new System.Windows.Forms.TextBox();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.buscarID = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbMarca
+            // 
+            this.tbMarca.Location = new System.Drawing.Point(52, 189);
+            this.tbMarca.Name = "tbMarca";
+            this.tbMarca.Size = new System.Drawing.Size(124, 20);
+            this.tbMarca.TabIndex = 0;
+            // 
+            // tbModelo
+            // 
+            this.tbModelo.Location = new System.Drawing.Point(182, 189);
+            this.tbModelo.Name = "tbModelo";
+            this.tbModelo.Size = new System.Drawing.Size(132, 20);
+            this.tbModelo.TabIndex = 1;
+            // 
+            // tbAno
+            // 
+            this.tbAno.Location = new System.Drawing.Point(320, 189);
+            this.tbAno.Name = "tbAno";
+            this.tbAno.Size = new System.Drawing.Size(100, 20);
+            this.tbAno.TabIndex = 2;
+            // 
+            // tbCor
+            // 
+            this.tbCor.Location = new System.Drawing.Point(426, 189);
+            this.tbCor.Name = "tbCor";
+            this.tbCor.Size = new System.Drawing.Size(100, 20);
+            this.tbCor.TabIndex = 3;
+            // 
+            // tbValor
+            // 
+            this.tbValor.Location = new System.Drawing.Point(532, 189);
+            this.tbValor.Name = "tbValor";
+            this.tbValor.Size = new System.Drawing.Size(100, 20);
+            this.tbValor.TabIndex = 5;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(663, 217);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 6;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(663, 246);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 7;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(663, 276);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(663, 306);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Marca";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(179, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Modelo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(317, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Ano";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(423, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Cor";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(529, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Valor";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(635, 170);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Categoria";
+            // 
+            // tbCategoria
+            // 
+            this.tbCategoria.Location = new System.Drawing.Point(639, 189);
+            this.tbCategoria.Name = "tbCategoria";
+            this.tbCategoria.Size = new System.Drawing.Size(100, 20);
+            this.tbCategoria.TabIndex = 17;
+            // 
+            // tbId
+            // 
+            this.tbId.Location = new System.Drawing.Point(52, 121);
+            this.tbId.Name = "tbId";
+            this.tbId.Size = new System.Drawing.Size(100, 20);
+            this.tbId.TabIndex = 18;
+            // 
+            // buscarID
+            // 
+            this.buscarID.AutoSize = true;
+            this.buscarID.Location = new System.Drawing.Point(52, 102);
+            this.buscarID.Name = "buscarID";
+            this.buscarID.Size = new System.Drawing.Size(72, 13);
+            this.buscarID.TabIndex = 19;
+            this.buscarID.Text = "Buscar por ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buscarID);
+            this.Controls.Add(this.tbId);
+            this.Controls.Add(this.tbCategoria);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.tbValor);
+            this.Controls.Add(this.tbCor);
+            this.Controls.Add(this.tbAno);
+            this.Controls.Add(this.tbModelo);
+            this.Controls.Add(this.tbMarca);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbMarca;
+        private System.Windows.Forms.TextBox tbModelo;
+        private System.Windows.Forms.TextBox tbAno;
+        private System.Windows.Forms.TextBox tbCor;
+        private System.Windows.Forms.TextBox tbValor;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbCategoria;
+        private System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.Label buscarID;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CRUD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection sqlCon = null;
+        private string strCon = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Usuario;Data Source=localhost\SQLEXPRESS";
+        private string strSql = string.Empty;
+
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            strSql = "EXEC CriarNovoModelo @Marca, @Modelo, @Ano, @Cor, @Valor, @Categoria";
+            sqlCon = new SqlConnection(strCon);
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+            comando.Parameters.Add("@Marca", SqlDbType.VarChar).Value = tbMarca.Text;
+            comando.Parameters.Add("@Modelo", SqlDbType.VarChar).Value = tbModelo.Text;
+            comando.Parameters.Add("@Ano", SqlDbType.Int).Value = tbAno.Text;
+            comando.Parameters.Add("@Cor", SqlDbType.VarChar).Value = tbCor.Text;
+            comando.Parameters.Add("@Valor", SqlDbType.Int).Value = tbValor.Text;
+            comando.Parameters.Add("@Categoria", SqlDbType.VarChar).Value = tbCategoria.Text;
+
+            try
+            {
+                sqlCon.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Cadastro realizado com sucesso!");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            finally
+            {
+                sqlCon.Close();
+            }
+
+
+
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            strSql = "SELECT * FROM EstoqueCarros WHERE Id=@Id ";
+            sqlCon = new SqlConnection(strCon);
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+            comando.Parameters.Add("@Id", SqlDbType.Int).Value = tbId.Text;
+
+            try
+            {
+                sqlCon.Open();
+                SqlDataReader dr = comando.ExecuteReader();
+                dr.Read();
+
+                tbId.Text = Convert.ToString(dr["Id"]);
+                tbMarca.Text = Convert.ToString(dr["Marca"]);
+                tbAno.Text = Convert.ToString(dr["Ano"]);
+                tbCor.Text = Convert.ToString(dr["Cor"]);
+                tbValor.Text = Convert.ToString(dr["Valor"]);
+                tbCategoria.Text = Convert.ToString(dr["Categoria"]);
+
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+              /* if (tbId.Text == string.Empty)
+                {
+                   MessageBox.Show("Digite um ID");
+                    tbId.SelectAll();
+                    tbId.Focus();
+                    return;
+                }
+               
+                if (dr.HasRows == false)
+                {
+                    MessageBox.Show("Id nao cadastrado");
+                    tbId.SelectAll();
+                    tbId.Focus();
+                    return;
+                }
+                sqlCon.Close();*/
+            }
+
+            finally { sqlCon.Close(); } 
+
+        }
+    }
+}
